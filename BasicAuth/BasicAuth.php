45aea27bf3bef8efe624bfdf87add9355a99da1b
David Schmidt <mail@davidschmidt.at>
2016-02-04 21:00:54 +0100

    4 space indentation

--- BasicAuth/BasicAuth.php
+++ BasicAuth/BasicAuth.php
@@ -1,31 +1,31 @@
 <?php
 
 class BasicAuthPlugin extends MantisPlugin {
-        function register() {
-                $this->name        = 'BasicAuth Plugin';
-                $this->description = 'Looks for REMOTE_USER in SERVER environment and autologins user.';
-                $this->version     = '0.01';
-                $this->requires    = array( 'MantisCore' => '1.2.0' );
-                $this->author      = 'David Schmidt';
-                $this->contact     = 'david.schmidt -at- univie.ac.at';
-                $this->url         = '';
-        }
+    function register() {
+        $this->name        = 'BasicAuth Plugin';
+        $this->description = 'Looks for REMOTE_USER in SERVER environment and autologins user.';
+        $this->version     = '0.01';
+        $this->requires    = array( 'MantisCore' => '1.2.0' );
+        $this->author      = 'David Schmidt';
+        $this->contact     = 'david.schmidt -at- univie.ac.at';
+        $this->url         = '';
+    }
 
-        function init() {
-                plugin_event_hook( 'EVENT_CORE_READY', 'autologin' );
-        }
+    function init() {
+        plugin_event_hook( 'EVENT_CORE_READY', 'autologin' );
+    }
 
-        function autologin() {
-                if (auth_is_user_authenticated())
-                        return;
+    function autologin() {
+        if (auth_is_user_authenticated())
+            return;
 
-                $t_user_id = user_get_id_by_name($_SERVER['REMOTE_USER']);
-                if ($t_user_id)   {
-                        user_increment_login_count( $t_user_id );
-                        user_reset_failed_login_count_to_zero( $t_user_id );
-                        user_reset_lost_password_in_progress_count_to_zero( $t_user_id );
-                        auth_set_cookies($t_user_id, true);
-                        auth_set_tokens($t_user_id);
-                }
+        $t_user_id = user_get_id_by_name($_SERVER['REMOTE_USER']);
+        if ($t_user_id)   {
+            user_increment_login_count( $t_user_id );
+            user_reset_failed_login_count_to_zero( $t_user_id );
+            user_reset_lost_password_in_progress_count_to_zero( $t_user_id );
+            auth_set_cookies($t_user_id, true);
+            auth_set_tokens($t_user_id);
         }
+    }
 }
