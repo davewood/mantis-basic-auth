82a1fa69cce0fc9130d8e478322964bafc8e46f7
David Schmidt <mail@davidschmidt.at>
2016-02-09 11:21:20 +0100

    check user validity and configured login method
    check login method is BASIC AUTH
    check if user is valid
    throw error otherwise

--- BasicAuth/BasicAuth.php
+++ BasicAuth/BasicAuth.php
@@ -19,13 +19,20 @@ class BasicAuthPlugin extends MantisPlugin {
         if (auth_is_user_authenticated())
             return;
 
-        $t_user_id = user_get_id_by_name($_SERVER['REMOTE_USER']);
-        if ($t_user_id)   {
-            user_increment_login_count( $t_user_id );
-            user_reset_failed_login_count_to_zero( $t_user_id );
-            user_reset_lost_password_in_progress_count_to_zero( $t_user_id );
-            auth_set_cookies($t_user_id, true);
-            auth_set_tokens($t_user_id);
+        $t_login_method = config_get( 'login_method' );
+        if ( $t_login_method != BASIC_AUTH ) {
+            trigger_error( "Invalid login method. ($t_login_method)", ERROR );
         }
+
+        $t_user_id = user_get_id_by_name($_SERVER['REMOTE_USER']);
+        if ( !$t_user_id ) {
+            trigger_error( 'Invalid user.', ERROR );
+        }
+
+        user_increment_login_count( $t_user_id );
+        user_reset_failed_login_count_to_zero( $t_user_id );
+        user_reset_lost_password_in_progress_count_to_zero( $t_user_id );
+        auth_set_cookies($t_user_id, true);
+        auth_set_tokens($t_user_id);
     }
 }
